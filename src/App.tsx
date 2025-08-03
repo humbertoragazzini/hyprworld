6bb01fe99c05ac8b48e344fae648efbf4c661c53
humberto ragazzini <beto_vicio@hotmail.com>
2025-08-03 22:04:05 +0000

    small change in the io icons and bar for the selected item

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,17 @@ function App() {
       <div className="w-full h-full text-[#96a4b2]">
         <div className="flex h-screen bg-gradient-to-b from-[#0f161c] to-[#212b35] text-gray-100 font-sans">
           {/* Sidebar */}
-          <aside className="w-18 bg-[#0f161c] flex flex-col items-center space-y-6 shadow-md border-r-2 border-[#1e343f]">
+          <aside className="w-18 bg-[#0f161c] flex flex-col items-center space-y-2 shadow-md border-r-2 border-[#1e343f]">
+            <div className="h-[68px] w-full left-0 top-0 bg-[radial-gradient(circle_at_top_left,_#2f3554,_#2f4542)] pb-[1px]">
+              <div className="bg-[#1e222c] h-[67px] w-full">
+                <button className="w-[50px] aspect-square m-2">
+                  <SlScreenDesktop
+                    fill="#ffffff"
+                    className="w-full h-1/2 fill-[#9eabb7] text-white transition-all duration-500"
+                  />
+                </button>
+              </div>
+            </div>
             <button className="hover:[&_svg]:scale-110 hover:bg-[rgba(250,250,250,0.4)] transition-all duration-500 w-[50px] aspect-square m-2 rounded-lg bg-[rgba(200,200,200,0.1)] cursor-pointer">
               <SlScreenDesktop
                 fill="#ffffff"
